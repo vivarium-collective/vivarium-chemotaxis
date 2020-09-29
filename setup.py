89f79c9bcf456716c66690de39bfe7bc3fe1837d
Eran Agmon <agmon.eran@gmail.com>
2020-09-28 17:12:55 -0700

    require vivarium-core==0.0.29

--- setup.py
+++ setup.py
@@ -25,5 +25,5 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
-        'vivarium-core==0.0.28',
+        'vivarium-core==0.0.29',
         'vivarium-cell==0.0.23'])
