1ccdc41e1f62080cb82e4fc7d55e494e9635f226
Eran Agmon <agmon.eran@gmail.com>
2020-10-04 15:05:19 -0700

    update core requirement

--- setup.py
+++ setup.py
@@ -25,5 +25,5 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
-        'vivarium-core==0.0.29',
+        'vivarium-core==0.0.32',
         'vivarium-cell==0.0.23'])
